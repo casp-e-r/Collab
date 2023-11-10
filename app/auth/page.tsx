6d4535b0af26523c92f88ec4a8fd23def79753b4
casp-e-r <aswinkarooth@gmail.com>
2023-11-10 23:13:30 +0530

    Added prisma - Established connection to MongoDb Atlas

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,25 @@
 'use client'
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
 import React, { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 
 function auth() {
     const [authType, setAuthType] = useState<"signin" | "signup">("signin");
     const [showPasswod, setshowPasswod] = useState<boolean>(false);
+
+    const { register: signInRegister,
+        handleSubmit: handleSignInSubmit,
+        formState: { errors } } = useForm<FieldValues>({
+        })
+    const { register: signUpRegister,
+        handleSubmit: handleSignUpSubmit,
+        formState: { errors: signUpErrors } } = useForm<FieldValues>({
+        })
+    const onSignInSubmit: SubmitHandler<FieldValues> = (data) => {
+        console.log(data);
+
+    }
 
     return (
         <div className='h-screen grid grid-flow-col'>
@@ -13,18 +27,21 @@ function auth() {
                 {authType == 'signin' ? (
                     <div className='my-10 pt-10 bg-inherit space-y-3'>
                         <FormControl variant="floating" className='bg-inherit'>
-                            <Input placeholder=" " />
+                            <Input placeholder=" " {...signInRegister("userName",{ required: true })} />
                             <FormLabel >user name</FormLabel>
                         </FormControl>
                         <div className='relative'>
                             <FormControl variant="floating" className='bg-inherit' >
-                                <Input type={showPasswod ? 'text' : 'password'} placeholder=" " />
+                                <Input type={showPasswod ? 'text' : 'password'} placeholder=" " {...signInRegister("password",{ required: true })} />
                                 <FormLabel >password</FormLabel>
                             </FormControl>
                             <span onClick={() => setshowPasswod(!showPasswod)} className='absolute right-0 top-2 z-30'>
                                 {showPasswod ? 'Hide' : 'Show'}
                             </span>
                         </div>
+                        <Button  loadingText='Signing in' onClick={handleSignInSubmit(onSignInSubmit)} >
+                            Sign in
+                        </Button>
                     </div>) : (
                     <div className='my-10 pt-10 bg-inherit'>
                         <FormControl variant="floating" className='bg-inherit'>
@@ -43,6 +60,9 @@ function auth() {
                             <Input placeholder=" " />
                             <FormLabel>re enter password</FormLabel>
                         </FormControl>
+                        <Button isLoading loadingText='Signing up'                         >
+                            Sign up
+                        </Button>
                     </div>
                 )}
 
